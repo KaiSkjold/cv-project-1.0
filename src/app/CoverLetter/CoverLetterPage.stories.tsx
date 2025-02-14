40c4cb26a1c3513ab4c642ae26fb34c2257ab241
KaiSkjold <skjoldann@gmail.com>
2025-02-14 14:00:14 +0100

    added stories for storybook main pages

---
+++ src/app/CoverLetter/CoverLetterPage.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react';
+import CoverLetterPage from './CoverLetterPage';
+
+const meta: Meta<typeof CoverLetterPage> = {
+    component: CoverLetterPage,
+};
+
+
+export default meta;
+type Story = StoryObj<typeof CoverLetterPage>;
+
+export const Default: Story = {};
---
+++ src/app/Resume/Components/ResumePage.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ResumePage from '../ResumePage';
+
+
+const meta: Meta<typeof ResumePage> = {
+    component: ResumePage,
+};
+
+
+export default meta;
+type Story = StoryObj<typeof ResumePage>;
+
+export const Default: Story = {};
